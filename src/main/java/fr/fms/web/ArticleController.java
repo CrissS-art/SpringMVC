67d6e21a624db8ffe7b7fb59635ad46e1a56627e
Chris Le_Jeune <christophe.lejeune@fms-ea.com>
2024-12-18 10:33:51 +0100

    Model-Vue implemented

---
+++ src/main/java/fr/fms/web/ArticleController.java
@@ -0,0 +1,28 @@
+package fr.fms.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import fr.fms.dao.ArticleRepository;
+import fr.fms.entities.Article;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ArticleController {
+	@Autowired
+	ArticleRepository articleRepository;
+	
+	@GetMapping("/index")
+	public String index(Model model) { //model is provided by spring
+		List<Article> articles = articleRepository.findAll();
+		model.addAttribute("listArticle",articles);
+			
+		return "articles"; //returns dispatcherServlet = vue, returns articles.html
+	}
+	
+}
